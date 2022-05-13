621cb605c01c85bc73f569a896fb55133a93369d
milanthakor_crest <milan.thakor@crestdatasys.com>
2022-05-13 15:04:51 +0530

    Implement a Root-Level Request Logger Middleware.

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
@@ -9,10 +10,16 @@ console.log('Hello World');
 // allows the client to access the files from the public folder via URL
 app.use('/public', express.static(__dirname + '/public'));
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
+
 // serving a json data
 app.get('/json', (req, res) => {
+    let message = 'Hello json';
     res.json({
-        'message': 'Hello json'
+        'message': process.env.MESSAGE_STYLE === 'uppercase' ? message.toUpperCase() : message
     });
 });
 
